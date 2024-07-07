2dec166f074871dbb02693b1f3f7e026ab80733e
MechaLarry <mecha.larry@gmail.com>
2024-07-07 21:59:03 +0200

    add files

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+
+//generate a random integer greater than zero
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4; // The remainder will be 4
+
+console.log(mod);
+
+
+// Variable to find the maximum number
+let max = Math.max(1, 5, 10, 20); // The highest number in the set is 20
+
+console.log(max);
